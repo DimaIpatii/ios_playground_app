930282da1b8cddf4438b53d422b21f9e09fcb8d6
Demetrius Ipatii <dima.ipatjj@gmail.com>
2025-10-25 17:00:20 +0200

    Initial Commit

---
+++ iOS Playground App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Playground App
+//
+//  Created by Dmytro Ipatii on 25/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Playground App/iOS_Playground_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Playground_AppApp.swift
+//  iOS Playground App
+//
+//  Created by Dmytro Ipatii on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Playground_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
